775528464a6c61c0e40fde055f56b945d00e3bf8
Marvoh254 <137793330+Marvoh254@users.noreply.github.com>
2025-05-06 23:16:19 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -61,7 +61,7 @@ export default function Login() {
           </form>
 
           <p className="text-gray-400 text-sm text-center mt-4">
-            {Don't have an account?}{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="text-[#d4af37] hover:underline">
               Sign up
             </Link>
